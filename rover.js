c28fb70946d2684df7267153c91b87b2904c740b
Yiqi Zhu (Cecilia) <zhuyiqi28@gmail.com>
2021-03-21 01:04:02 +0000

    homework

--- rover.js
+++ rover.js
@@ -1,5 +1,39 @@
 class Rover {
    // Write code here!
+ constructor(position){
+      this.position=position;
+      this.mode="Normal";
+      this.generatorWatts=110;
+  }
+    receiveMessage(message){
+    let results=[];
+
+    for (let i=0; i<message.commands.length;i++){
+      if (message.commands[i].commendType='STATUS_CHECK'){
+        results.push({completed: true,
+        roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position:this.position}})
+      
+      }else if (message.commands[i].commendType="MODE_CHANGE"){
+      this.mode=message.commands[i].value;
+      results.push({completed:true})
+      
+      }else if (message.commands[i].commendType="MOVE"){
+      if(this.mode=="LOW_POWER"){
+        results.push({completed:false})
+        
+        }else if (this.mode=="Normal"){
+          this.position=message.commands[i].value;
+          results.push({completed:true})
+        }
+    }
+    }
+return{
+  message:message.name,
+  results:results
+}
+
+   // Write code here!
+}
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,67 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  // 7 tests here
+  it("constructor sets position and default values for mode and generatorWatts.",function() {
+    let rover=new Rover(123);
+    expect(rover.position).toEqual(123);
+    expect(rover.mode).toEqual("Normal");
+    expect(rover.generatorWatts).toEqual(110);
+    });
+    //test 8
 
-});
+    it("response returned by receiveMessage contains name of message",function(){
+      let rover=new Rover(123);
+      let command=[new Command("abc")];
+      let message=new Message("name of message",command);
+      let response=rover.receiveMessage(message)
+      expect(response.message).toEqual("name of message");
+    });
+
+    // test 9
+    it("response returned by receiveMessage includes two results if two commands are sent in the message" ,function(){
+      let rover=new Rover(123);
+      let command=[new Command("STATUS_CHECK"),new Command("STATUS_CHECK")];
+      let message=new Message("two message",command);
+      let response=rover.receiveMessage(message);
+      expect(response.results.length).toEqual(2);
+      });
+
+    // test 10
+    it("responds correctly to status check command", function(){
+      let rover=new Rover(123);
+      let command=[new Command("STATUS_CHECK")];
+      let message=new Message("one comend",command);
+      let response=rover.receiveMessage(message);
+      expect(response.results.roverStatus).toEqual({completed: true, roverStatus: {mode: 'NORMAL', generatorWatts: 110, position: 123}} );
+      });
+    //test 11
+    it("responds correctly to status check command", function(){
+      let rover=new Rover(123);
+      let command=[new Command("MODE_CHANGE","LOW_POWER")];
+      let message=new Message("one comend",command);
+      let response=rover.receiveMessage(message);
+      expect(response.results).toEqual([{completed: true}]);
+      excpect(rover.mode).toEqual('LOW_POWER')
+    })
+    //test 12
+   it("responds with false completed value when attempting to move in LOW_POWER mode", function(){
+      let rover=new Rover(123);
+      let command=[new Command("MODE_CHANGE","LOW_POWER"),new Command("Move",321)];
+      let message=new Message("two comend",command);
+      let response=rover.receiveMessage(message);
+      expect(response.results).toEqual([{completed: true}],[{completed: false}]);
+      excpect(rover.position).toEqual(123);
+    })
+    //test 13
+  it("responds with position for move command", function(){
+      let rover=new Rover(123);
+      let command=[new Command("Move",321)];
+      let message=new Message("one comend",command);
+      let response=rover.receiveMessage(message);
+      expect(response.results).toEqual([{completed: true}])
+      expect(rover.position).toEqual(321)
+    })
+
+  })
+
